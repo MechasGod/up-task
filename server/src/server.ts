65fc8665f5cabe74cac8d5106bbfa3d550cc1338
MechasGod <juanmunozr@unicauca.edu.co>
2024-07-06 12:59:08 -0500

    set CORS config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,21 @@
 import express from "express"
 import { connectToDb } from './config/db';
 import projectsRouter from './routes/projectRoutes';
+import cors from "cors"
+import { corsConfig } from './config/cors';
+
 
 const app = express()
 
 //Express Configs
 app.use(express.json())
-app.disable("")
+app.disable("x-powered-by")
 
 //DB
 connectToDb()
+
+//CORS
+app.use(cors(corsConfig))
 
 //Routes
 app.use("/projects", projectsRouter)
